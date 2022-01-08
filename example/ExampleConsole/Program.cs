0804be858f8607e7fab404fbee2e9861430b579b
onlly Archibald <412102100@qq.com>
2022-01-08 20:48:12 +0800

    update file download example

--- example/ExampleConsole/Program.cs
+++ example/ExampleConsole/Program.cs
@@ -5,31 +5,46 @@ using Newtonsoft.Json;
 using Retrofit.Net.Core;
 using Retrofit.Net.Core.Converts;
 using Retrofit.Net.Core.Models;
+using ShellProgressBar;
 
 var client = new RetrofitClient.Builder()
     .AddInterceptor(new HeaderInterceptor())
     .AddInterceptor(new SimpleInterceptorDemo())
+    .AddTimeout(TimeSpan.FromSeconds(10)) // The default wait time after making an http request is 6 seconds
     .Build();
 var retrofit = new Retrofit.Net.Core.Retrofit.Builder()
-    .AddBaseUrl("https://localhost:7283")
+    .AddBaseUrl("https://localhost:7283") // Base Url
     .AddClient(client)
     .AddConverter(new DefaultXmlConverter()) // The internal default is ‘DefaultJsonConverter’ if you don’t call ‘.AddConverter(new DefaultJsonConverter())’
     .Build();
 var service = retrofit.Create<IPersonService>();
 
-
-
-/*Response<Stream> response = await service.Download("test");
+Response<Stream> response = await service.Download("test");
 Stream outStream = File.Create("/Users/onllyarchibald/Desktop/a.zip");
+
+var options = new ProgressBarOptions
+{
+    ForegroundColor = ConsoleColor.Yellow,
+    ForegroundColorDone = ConsoleColor.DarkGreen,
+    BackgroundColor = ConsoleColor.DarkGray,
+    BackgroundCharacter = '\u2593'
+};
+var pbar = new ProgressBar((int)response.Body!.Length, "File Downloading", options);
+
 byte[] buffer = new byte[1024];
 int i;
-do{
-    i = response.Body!.Read(buffer,0,buffer.Length);
-    if(i > 0)outStream.Write(buffer,0,i);
-}while(i > 0);
+do
+{
+    i = response.Body!.Read(buffer, 0, buffer.Length);
+    if (i > 0)
+    {
+        outStream.Write(buffer, 0, i);
+        pbar.Tick((int)outStream.Length,"Downloading...");
+    }
+} while (i > 0);
 outStream.Close();
 response.Body.Close();
-Console.WriteLine("File upload completed...");*/
+Console.WriteLine("\nFile download completed...");
 
 // HttpClient client1 = new HttpClient();
 // Stream inStream = await client1.GetStreamAsync("https://localhost:7283/WeatherForecast");
